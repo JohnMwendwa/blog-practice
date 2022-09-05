8f6630afab15d32bb944504489a93daf4c2e3101
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-05 21:28:15 +0300

    import and render the search component

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -1,28 +1,34 @@
 import React from "react";
 import Link from "next/link";
+import Search from "../search/Search";
 
 export default function Navbar() {
   return (
     <nav>
       <div>
-        <Link href="/">
-          <a> Mwendwa&quot;s Blog</a>
-        </Link>
-      </div>
+        <div>
+          <Link href="/">
+            <a> Mwendwa&quot;s Blog</a>
+          </Link>
+        </div>
 
-      <ul>
-        <li>
-          <Link href="/posts">
-            <a> Posts</a>
-          </Link>
-          <Link href="/about">
-            <a> About</a>
-          </Link>
-          <Link href="/contact">
-            <a> Contact</a>
-          </Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link href="/posts">
+              <a> Posts</a>
+            </Link>
+            <Link href="/about">
+              <a> About</a>
+            </Link>
+            <Link href="/contact">
+              <a> Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Search searchTerm={() => null} />
+      </div>
     </nav>
   );
 }

e116681e555baeb85fe5f7fe020e9c16e8a79edf
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-05 20:38:50 +0300

    create the navbar

---
+++ components/navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <a> Mwendwa&quot;s Blog</a>
+        </Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link href="/posts">
+            <a> Posts</a>
+          </Link>
+          <Link href="/about">
+            <a> About</a>
+          </Link>
+          <Link href="/contact">
+            <a> Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}

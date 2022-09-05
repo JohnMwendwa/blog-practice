8d87313900c52f8ba45d0beb2dd71d10d0988570
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-05 21:39:29 +0300

    import the footer component and render it

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Footer from "./footer/Footer";
 import Navbar from "./navbar/Navbar";
 
-export default function Layout(props) {
-  return <Navbar />;
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main> {children}</main>
+      <Footer />
+    </>
+  );
 }

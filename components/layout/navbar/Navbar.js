34f94e3e6c233048f6aa7f644bb755dba0499db8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-06 06:48:53 +0300

    style the navbar component

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -1,34 +1,72 @@
 import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+
 import Search from "../search/Search";
+
+const NavWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 20px 10px;
+  background-color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const NavigationItems = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const ListItems = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  margin-left: 8px;
+  font-size: 18px;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-size: 32px;
+`;
 
 export default function Navbar() {
   return (
-    <nav>
-      <div>
-        <div>
+    <NavWrapper>
+      <NavigationItems>
+        <Logo>
           <Link href="/">
-            <a> Mwendwa&quot;s Blog</a>
+            <a> TECHme</a>
           </Link>
-        </div>
+        </Logo>
 
-        <ul>
-          <li>
+        <ListItems>
+          <ListItem>
             <Link href="/posts">
               <a> Posts</a>
             </Link>
+          </ListItem>
+          <ListItem>
             <Link href="/about">
               <a> About</a>
             </Link>
+          </ListItem>
+          <ListItem>
             <Link href="/contact">
               <a> Contact</a>
             </Link>
-          </li>
-        </ul>
-      </div>
-      <div>
+          </ListItem>
+        </ListItems>
+      </NavigationItems>
+      <SearchContainer>
         <Search searchTerm={() => null} />
-      </div>
-    </nav>
+      </SearchContainer>
+    </NavWrapper>
   );
 }

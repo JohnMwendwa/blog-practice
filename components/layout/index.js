7b6a2e52978e779f1001cc9f9da4adb618ab16ea
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-07 17:47:41 +0300

    use flex on the body layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import Footer from "./footer/Footer";
 import Navbar from "./navbar/Navbar";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 export default function Layout({ children }) {
   return (
-    <>
+    <Container>
       <Navbar />
       <main> {children}</main>
       <Footer />
-    </>
+    </Container>
   );
 }

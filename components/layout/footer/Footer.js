ae985146505ab5cb9855055b1fdf731012f36130
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-07 18:11:50 +0300

    style the footer

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -1,5 +1,29 @@
 import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: auto;
+  padding: 20px;
+  height: 80px;
+  border-top: 1px solid black;
+`;
+const Copyright = styled.div``;
+const Info = styled.div``;
 
 export default function Footer() {
-  return <div>Footer</div>;
+  const date = new Date().getFullYear();
+  return (
+    <FooterWrapper>
+      <Copyright>&copy; {date}</Copyright>
+      <Info>
+        <Link href="mailto:dev.johnmwendwa@gmail.com">
+          <a>John Mwendwa</a>
+        </Link>
+      </Info>
+    </FooterWrapper>
+  );
 }

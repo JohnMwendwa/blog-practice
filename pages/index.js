00ffdf666577cf7c6479c357692e0d3bf706b744
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 16:26:08 +0300

    render empty element if there ae no featured posts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { getFeaturedPosts } from "../helpers/posts_utils";
 import FeaturedPosts from "../components/homepage/FeaturedPosts";
 
 export default function Home({ posts }) {
+  if (!posts.length) return <></>;
+
   return (
     <div>
       <FeaturedPosts posts={posts} />

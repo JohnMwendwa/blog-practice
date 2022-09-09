5ee9515f7a6323510fe54bf52d9da90f2bf8b735
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 09:45:21 +0300

    style the allposts container

--- components/posts/AllPosts.js
+++ components/posts/AllPosts.js
@@ -6,15 +6,24 @@ const Title = styled.h1`
   text-align: center;
 `;
 
+const PostsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  row-gap: 0px;
+  column-gap: 1.2rem;
+  justify-items: center;
+  align-content: center;
+`;
+
 export default function AllPosts({ posts }) {
   return (
     <>
       <Title>All Posts</Title>
-      <div>
+      <PostsContainer>
         {posts.map((post) => (
           <PostItem key={post.slug} post={post} />
         ))}
-      </div>
+      </PostsContainer>
     </>
   );
 }

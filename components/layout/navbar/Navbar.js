bdc9aca6c35b4603e5728ac1aa1994125324f4e9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 19:04:10 +0300

    change the link for the posts to go to /blog instead of /posts

--- components/layout/navbar/Navbar.js
+++ components/layout/navbar/Navbar.js
@@ -61,7 +61,7 @@ export default function Navbar() {
       </NavigationItems>
       <ListItems>
         <ListItem>
-          <Link href="/posts">
+          <Link href="/blog">
             <a> Posts</a>
           </Link>
         </ListItem>

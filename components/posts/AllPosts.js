a3ad9c08b9f71d6a57d4b72a53616c823c9c701d
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 09:43:09 +0300

    map through all posts to render each individual post

--- components/posts/AllPosts.js
+++ components/posts/AllPosts.js
@@ -6,24 +6,15 @@ const Title = styled.h1`
   text-align: center;
 `;
 
-const PostsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  row-gap: 0px;
-  column-gap: 1.2rem;
-  justify-items: center;
-  align-content: center;
-`;
-
 export default function AllPosts({ posts }) {
   return (
     <>
       <Title>All Posts</Title>
-      <PostsContainer>
+      <div>
         {posts.map((post) => (
           <PostItem key={post.slug} post={post} />
         ))}
-      </PostsContainer>
+      </div>
     </>
   );
 }

34d91e833c31171e9806c1d1278886713ac80920
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 16:21:45 +0300

    import getFeaturedPosts function and fetch the featured posts

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div></div>;
+import { getFeaturedPosts } from "../helpers/posts_utils";
+import FeaturedPosts from "../components/homepage/FeaturedPosts";
+
+export default function Home({ posts }) {
+  return (
+    <div>
+      <FeaturedPosts posts={posts} />
+    </div>
+  );
 }
+
+export const getStaticProps = () => {
+  const posts = getFeaturedPosts();
+  return {
+    props: {
+      posts,
+    },
+  };
+};

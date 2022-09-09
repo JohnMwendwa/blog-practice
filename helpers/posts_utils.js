baa88e04dd9c79adc696c6f4fdfcb860577ada64
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-09 16:13:04 +0300

    create methods to fetch post grey matter and content

---
+++ helpers/posts_utils.js
@@ -0,0 +1,39 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+const getPostFiles = () => fs.readdirSync(postsDirectory);
+
+export const getPostData = (fileName) => {
+  const slug = fileName.replace(/\.md$/, "");
+
+  const filePath = path.join(postsDirectory, `${slug}.md`);
+
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+
+  const { data, content } = matter(fileContent);
+
+  return {
+    slug,
+    data,
+    content,
+  };
+};
+
+export const getAllPosts = () => {
+  const postFiles = getPostFiles();
+  const allPosts = postFiles.map((post) => getPostData(post));
+
+  //   sort posts by date
+  const sortedPosts = allPosts.sort((postA, postB) =>
+    postA.date > postB.date ? -1 : 1
+  );
+
+  return sortedPosts;
+};
+
+export const getFeaturedPosts = () => {
+  return getAllPosts().filter((post) => post.isFeatured);
+};

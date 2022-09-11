e7d217970ce5be00894bbc1bd8f25c8e32627cd2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-11 15:36:44 +0300

    add content to the about.js file

---
+++ components/about/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const AboutWrapper = styled.div``;
+const Title = styled.h1`
+  font-size: 28px;
+`;
+
+export default function About() {
+  return (
+    <AboutWrapper>
+      <Title>About Us</Title>
+    </AboutWrapper>
+  );
+}

50807cff088f5a728cc95aff4a11452d4bce1314
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-11 21:42:14 +0300

    render an animatimg title and content

--- components/about/About.js
+++ components/about/About.js
@@ -1,15 +1,35 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 import styled from "styled-components";
 
-const AboutWrapper = styled.div``;
+const AboutWrapper = styled.div`
+  width: 95%;
+  max-width: 60rem;
+  padding: 1rem;
+  margin: 2rem auto;
+  font-size: 1.25rem;
+`;
 const Title = styled.h1`
-  font-size: 28px;
+  font-size: 48px;
+  margin: 0;
+  padding: 32px 0;
+  text-align: center;
 `;
 
 export default function About() {
   return (
     <AboutWrapper>
-      <Title>About Us</Title>
+      <Title>
+        <TypeAnimation
+          sequence={["About Us", 1000, "", 500]}
+          repeat={Infinity}
+          speed={20}
+        />
+      </Title>
+      <p>
+        We value your time, that&apos;s why we keep our content short, precise
+        and easy to understand.
+      </p>
     </AboutWrapper>
   );
 }

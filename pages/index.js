7f842805abb41b5bdd14361f62e2f1542e508a45
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-12 16:42:17 +0300

    add title and description metatag for the index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
+import Head from "next/head";
 import { getFeaturedPosts } from "../helpers/posts_utils";
 import FeaturedPosts from "../components/homepage/FeaturedPosts";
 
 export default function Home({ posts }) {
-  if (!posts.length) return <></>;
+  if (!posts.length)
+    return (
+      <>
+        <Head>
+          <title>John Mwendwa blog post</title>
+          <meta
+            name="description"
+            content="I post about web development and best programming practices "
+          />
+        </Head>
+      </>
+    );
 
   return (
-    <div>
+    <>
+      <Head>
+        <title>John Mwendwa blog post</title>
+        <meta
+          name="description"
+          content="I post about web development and best programming practices "
+        />
+      </Head>
       <FeaturedPosts posts={posts} />
-    </div>
+    </>
   );
 }
 

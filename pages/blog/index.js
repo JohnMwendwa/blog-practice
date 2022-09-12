0bd6634ea1a4f24ad21f07fdaec806a1b6f7630b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-12 16:49:20 +0300

    add title and decription meta tag for the all posts page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,33 @@
+import Head from "next/head";
 import { getAllPosts } from "../../helpers/posts_utils";
 import AllPosts from "../../components/posts/AllPosts";
 
 export default function AllBlogPosts({ posts }) {
-  if (!posts.length) return <></>;
+  if (!posts.length)
+    return (
+      <>
+        <Head>
+          <title>All Posts</title>
+          <meta
+            name="description"
+            content="list of all programming related tutorials"
+          />
+        </Head>
+      </>
+    );
 
-  return <AllPosts posts={posts} />;
+  return (
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="list of all programming related tutorials"
+        />
+      </Head>
+      <AllPosts posts={posts} />
+    </>
+  );
 }
 
 export const getStaticProps = () => {

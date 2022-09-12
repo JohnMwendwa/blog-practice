5acf277398d0c40cbbce4dc85348962d088917fc
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-12 16:36:54 +0300

    add default head meta tag for the blog

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
-import "../styles/globals.css";
+
 import Layout from "../components/layout";
 import { theme } from "../components/theme";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>

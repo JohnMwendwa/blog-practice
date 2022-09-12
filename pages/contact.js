ec95975436afbc2d011074a6de96400d5f4e329e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-12 16:45:31 +0300

    add title and description metatag for the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Head from "next/head";
 import ContactForm from "../components/contact/ContactForm";
 
 export default function ContactPage() {
-  return <ContactForm />;
+  return (
+    <>
+      <Head>
+        <title>Contact us</title>
+        <meta name="description" content="Send me your feedback" />
+      </Head>
+      <ContactForm />
+    </>
+  );
 }

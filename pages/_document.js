72cc0afe87750d4e6718181c61b406d7801bd8f1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-12 08:01:04 +0300

    create the _document.js file to portal the notification component to the ui

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import Document, { Html, Main, NextScript, Head } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <div id="notifications"></div>
+        </body>
+      </Html>
+    );
+  }
+}

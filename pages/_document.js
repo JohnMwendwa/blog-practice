6e48348b8a2d9d83420e967b1e52bebf412c078a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-27 00:33:20 +0300

    remove unnecesary markup

--- pages/_document.js
+++ pages/_document.js
@@ -1,33 +1,6 @@
-import Document, {
-  Html,
-  Main,
-  NextScript,
-  Head,
-  DocumentContext,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Main, NextScript, Head } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
   render() {
     return (
       <Html lang="en">

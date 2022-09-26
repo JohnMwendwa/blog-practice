4da6065e891ae29aa123eb78c44cfbc5c026a7c8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-27 00:40:29 +0300

    change the favicon link to relative

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Head>
           <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <link rel="icon" href="favicon.png" type="image/png" />
+          <link rel="icon" href="/favicon.png" type="image/png" />
         </Head>
         <Component {...pageProps} />
       </Layout>

3c89dcfbe13dbe26d5b0bb89e003e28ed6f3947a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-09-27 13:47:53 +0300

    add post titles as meta description content

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { getFeaturedPosts } from "../helpers/posts_utils";
 import FeaturedPosts from "../components/homepage/FeaturedPosts";
 
 export default function Home({ posts }) {
+  const postTitles = posts.map((post) => post.title);
+
   if (!posts.length)
     return (
       <>
@@ -20,10 +22,7 @@ export default function Home({ posts }) {
     <>
       <Head>
         <title>John Mwendwa blog post</title>
-        <meta
-          name="description"
-          content="I post about web development and best programming practices "
-        />
+        <meta name="description" content={`John Mwendwa,${postTitles}`} />
       </Head>
       <FeaturedPosts posts={posts} />
     </>

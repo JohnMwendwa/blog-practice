001d150bef60411c0456dfaa30444e81a117709a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-17 23:04:16 +0300

    create a search context and its provider

--- components/contexts/searchContext.js
+++ components/contexts/searchContext.js
@@ -1,1 +1,14 @@
+import React, { createContext, useState } from "react";
 
+export const SearchContext = createContext();
+
+export const SearchContextProvider = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+
+  return (
+    <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+      {props.children}
+    </SearchContext.Provider>
+  );
+};

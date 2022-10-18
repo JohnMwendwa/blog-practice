e98e910dad8678fab584253574d045301042b9c4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-18 15:38:58 +0300

    move the search context provider higher up the document element

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,21 @@ import { ThemeProvider } from "styled-components";
 
 import Layout from "../components/layout";
 import { theme } from "../components/theme";
+import { SearchContextProvider } from "../components/contexts/searchContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Head>
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <link rel="icon" href="/favicon.png" type="image/png" />
-        </Head>
-        <Component {...pageProps} />
-      </Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <SearchContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SearchContextProvider>
     </ThemeProvider>
   );
 }

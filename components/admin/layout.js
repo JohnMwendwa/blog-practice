eb1162888ec5a18ff79c7b0ff3f40514e21b4f87
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-19 17:16:21 +0300

    import and render the messages component

--- components/admin/layout.js
+++ components/admin/layout.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Messages from "./Messages";
 
 const Container = styled.div`
   display: flex;
@@ -39,6 +40,7 @@ export default function Layout() {
       </Sidebar>
       <Main>
         <h1>Notifications</h1>
+        <Messages />
       </Main>
     </Container>
   );

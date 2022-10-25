bb1ff459e40066612f6ed0a6c406cbb159ed996f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-25 21:43:50 +0300

    render different layouts for different routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
+import { useRouter } from "next/router";
 
 import Layout from "../components/layout";
 import { theme } from "../components/theme";
 import { SearchContextProvider } from "../components/contexts/searchContext";
 import "../styles/globals.css";
+import AdminLayout from "../components/admin/layout";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
@@ -15,7 +19,17 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <SearchContextProvider>
         <Layout>
-          <Component {...pageProps} />
+          {router.pathname === "/admin/messages" ||
+          router.pathname === "/admin/users" ||
+          router.pathname === "/admin/articles" ||
+          router.pathname === "/admin/settings" ||
+          router.pathname === "/admin/logout" ? (
+            <AdminLayout>
+              <Component {...pageProps} />
+            </AdminLayout>
+          ) : (
+            <Component {...pageProps} />
+          )}
         </Layout>
       </SearchContextProvider>
     </ThemeProvider>

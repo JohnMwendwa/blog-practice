2db5b196ec7252869b3b7de7ae9978a41a992920
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-26 22:13:30 +0300

    manage sign in with next-auth credentials provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+import { verifyPassword } from "../../../helpers/db/auth";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+    maxAge: 60 * 60,
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        connectToDatabase();
+
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) {
+          closeConnection();
+          throw new Error("User doesn't exist");
+        }
+
+        const isMatch = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isMatch) {
+          closeConnection();
+          throw new Error("Wrong password");
+        }
+
+        closeConnection();
+
+        return {
+          email: user.email,
+          isAdmin: user.isAdmin,
+          firstName: user.firstName,
+        };
+      },
+    }),
+  ],
+});

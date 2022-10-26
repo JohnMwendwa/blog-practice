f21485af6a688e406f2e92d8cf8a8a04ae5464df
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-26 21:39:24 +0300

    add method to compared password against hashed password stored in the db

--- helpers/db/auth.js
+++ helpers/db/auth.js
@@ -1,7 +1,13 @@
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export async function hashPassword(password) {
   const hashedPassword = await hash(password, 8);
 
   return hashedPassword;
+}
+
+export async function verifyPassword(password, hashedPassword) {
+  const isMatch = await compare(password, hashedPassword);
+
+  return isMatch;
 }

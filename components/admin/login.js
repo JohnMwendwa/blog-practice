8e4c029d848c64ce7d4a4885513537312a5b051f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-27 17:47:54 +0300

    style the login form

---
+++ components/admin/login.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  height: 60vh;
+
+  & h2 {
+    font-size: 2.5rem;
+    font-family: jokerman;
+    letter-spacing: 0.15rem;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+  }
+`;
+const Form = styled.form``;
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  outline: 1px solid ${(c) => c.theme.colors.ui.secondary};
+  margin: 5px 0;
+  text-align: center;
+`;
+const Btn = styled.button`
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  margin: 10px 0;
+  font-size: 1.2rem;
+  background-color: ${(c) => c.theme.colors.ui.secondary};
+  color: white;
+`;
+
+export default function Login() {
+  return (
+    <FormWrapper>
+      <h2>Login</h2>
+      <Form>
+        <Input type="email" />
+        <Input type="password" />
+        <Btn>Login</Btn>
+      </Form>
+    </FormWrapper>
+  );
+}

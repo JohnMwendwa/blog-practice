0b3191ff07e21484acaf01dd6682e2d62d51b9e9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-27 19:13:38 +0300

    redirect user based n active session

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,17 @@
-import AdminPanel from "../../components/admin/index";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
-export default function MessagesPage() {
-  return <AdminPanel />;
+export default function AdminPage() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/admin/messages", { permanent: true });
+    } else {
+      router.push("/admin/login", { permanent: true });
+    }
+  }, [router, session]);
+  return;
 }

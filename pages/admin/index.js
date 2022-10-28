ff98ad4c8939f7b89a33b8d7fbd4b94e02c05579
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-28 19:20:28 +0300

    substitute client side authentication for server side

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,26 @@
-import AdminPanel from "../../components/admin";
+import { getSession } from "next-auth/react";
 
 export default function AdminPage() {
-  return <AdminPanel />;
+  return;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+      redirect: {
+        destination: "/admin/messages",
+        permanent: true,
+      },
+    };
+  }
 }

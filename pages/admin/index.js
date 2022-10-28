1559340bad9f2e30f36d997cd2f2a4d8a84b867f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-28 17:12:27 +0300

    render the admin opanel component

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,17 +1,5 @@
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import AdminPanel from "../../components/admin";
 
 export default function AdminPage() {
-  const router = useRouter();
-  const { data: session } = useSession();
-
-  useEffect(() => {
-    if (session) {
-      router.push("/admin/messages", { permanent: true });
-    } else {
-      router.push("/admin/login", { permanent: true });
-    }
-  }, [router, session]);
-  return;
+  return <AdminPanel />;
 }

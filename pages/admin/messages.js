9329bdec3eeb5c425454f24f5086a35ce491d856
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-28 19:23:49 +0300

    add server side authentication

--- pages/admin/messages.js
+++ pages/admin/messages.js
@@ -1,5 +1,24 @@
+import { getSession } from "next-auth/react";
+
 import Messages from "../../components/admin/Messages";
 
 export default function MessagesPage() {
   return <Messages />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+    };
+  }
 }

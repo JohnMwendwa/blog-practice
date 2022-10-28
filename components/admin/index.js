14b8b2c1802614f368d936c9b0b1b31ec93e14c8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-28 17:11:45 +0300

    create admin panel index component

--- components/admin/index.js
+++ components/admin/index.js
@@ -1,5 +1,17 @@
-import Layout from "./layout";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
 export default function AdminPanel() {
-  return <Layout />;
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      router.push("/admin/messages");
+    } else {
+      router.push("/admin/login");
+    }
+  }, [router, session]);
+  return;
 }

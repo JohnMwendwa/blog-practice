652f3c0d68adb84cdf9c48f134d0609e040f53c7
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-28 19:27:45 +0300

    add server side authentication

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -3,3 +3,20 @@ import Users from "../../components/admin/users";
 export default function UsersPage() {
   return <Users />;
 }
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+    };
+  }
+}

2c6a1a79ebe5a01dca0084a6aa238c33540a32e4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-29 14:26:05 +0300

    fetch users

--- components/admin/users/index.js
+++ components/admin/users/index.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Link from "next/link";
 
 const Wrapper = styled.div``;
 
-export default function Users({ users = [] }) {
+export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch("/api/users");
+    const data = await response.json();
+    setUsers(data);
+  };
+
   return (
     <Wrapper>
       <h2>Users</h2>

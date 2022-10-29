2811fcd20d4dbcefb7bd0edf3a8e7a13a4b1aac5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-29 14:00:25 +0300

    style the user componennt

--- components/admin/users/user.js
+++ components/admin/users/user.js
@@ -1,5 +1,96 @@
-import React from "react";
+import styled from "styled-components";
 
-export default function User() {
-  return <div>User</div>;
+const Wrapper = styled.div`
+  & h2 {
+    font-family: jokerman;
+  }
+`;
+const Table = styled.table`
+  border: 1px solid #333;
+  border-collapse: collapse;
+  width: 80%;
+  margin: 0 auto;
+
+  & th,
+  td {
+    border: 1px solid #333;
+    padding: 8px;
+  }
+  & td:first-child {
+    font-weight: bold;
+  }
+
+  & tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+  & tr:hover {
+    background-color: #ddd;
+  }
+
+  & th {
+    background-color: ${(c) => c.theme.colors.ui.secondary};
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    color: white;
+  }
+
+  & button {
+    cursor: pointer;
+    background-color: #04aa6d;
+    color: white;
+    padding: 5px 20px;
+    border: none;
+  }
+`;
+
+export default function User({ user = {} }) {
+  const date = new Date(user.createdOn).toDateString();
+
+  return (
+    <Wrapper>
+      <h2>
+        {user.firstName} {user.lastName}
+      </h2>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Details</th>
+            <th>Function</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Email</td>
+            <td>{user.email}</td>
+            <td>
+              <button>Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <td>isAdmin</td>
+            <td>{user.isAdmin ? "True" : "False"} </td>
+            <td>
+              <button>Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <td>isAuthenticated</td>
+            <td>{user.isAuthenticated ? "True" : "False"}</td>
+            <td>
+              <button>Edit</button>
+            </td>
+          </tr>
+          <tr>
+            <td>CreatedOn</td>
+            <td>{date}</td>
+            <td>
+              <button>Edit</button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Wrapper>
+  );
 }

32ae9bdfc0b8082fdd304adc0e589c2604d2d617
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-29 22:13:57 +0300

    enable time stamps

--- helpers/db/models/message.js
+++ helpers/db/models/message.js
@@ -2,32 +2,34 @@ import mongoose from "mongoose";
 import validator from "validator";
 const { Schema } = mongoose;
 
-const messageSchema = new Schema({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid!");
-      }
+const messageSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid!");
+        }
+      },
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+      validate(value) {
+        if (value.trim() === "") {
+          throw new Error("You can't send a blank message");
+        }
+      },
     },
   },
-  name: {
-    type: String,
-    required: [true, "name is required"],
-  },
-  message: {
-    type: String,
-    required: [true, "Message is required"],
-    validate(value) {
-      if (value.trim() === "") {
-        throw new Error("You can't send a blank message");
-      }
-    },
-  },
-  Date: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 const Message =
   mongoose.models.Message || mongoose.model("Message", messageSchema);

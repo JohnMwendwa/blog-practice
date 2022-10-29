0743b877ced6456f9b50e74130e533e7202d64ec
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-29 20:14:09 +0300

    redirect to login page once the user logs out

--- pages/admin/users/[id].js
+++ pages/admin/users/[id].js
@@ -1,5 +1,23 @@
+import { getSession } from "next-auth/react";
 import User from "../../../components/admin/users/user";
 
 export default function UserPage({ user }) {
   return <User user={user} />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+    };
+  }
 }

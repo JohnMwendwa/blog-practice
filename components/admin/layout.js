ec1ef6657c96c755415c832b194d753c30ae5399
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-30 14:20:04 +0300

    change admin page layout

--- components/admin/layout.js
+++ components/admin/layout.js
@@ -1,11 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
 import { signOut, useSession } from "next-auth/react";
+import Footer from "../layout/footer/Footer";
 
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: ${(c) => c.theme.colors.ui.primary};
+  padding: 20px 30px;
+`;
+const Profile = styled.div``;
+const Avatar = styled(Image)`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  background-color: #333;
+`;
+
+const Logo = styled.h2`
+  font-family: jokerman;
+  font-size: 2.5rem;
+  color: white;
+  margin: 0;
+`;
 const Container = styled.div`
   display: flex;
-  height: 70vh;
+  height: 80vh;
 `;
+
 const Sidebar = styled.div`
   background-color: grey;
   width: 150px;
@@ -35,31 +58,44 @@ export default function Layout({ children }) {
   };
 
   return (
-    <Container>
-      <Sidebar>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/admin/messages">Messages</Link>
-            </li>
-            {session.user?.name.isAdmin && (
+    <>
+      <Header>
+        <Logo>TECHme</Logo>
+        <Profile>
+          <h3>John Mwendwa</h3>
+        </Profile>
+      </Header>
+
+      <Container>
+        <Sidebar>
+          <nav>
+            <ul>
               <li>
-                <Link href="/admin/users">Users</Link>
+                <Link href="/admin/dashbord">Dashboard</Link>
               </li>
-            )}
-            <li>
-              <Link href="/admin/articles">Articles</Link>
-            </li>
-            <li>
-              <Link href="/admin/settings">Settings</Link>
-            </li>
-            <li onClick={logoutHandler}>
-              <Link href="/admin/logout">Logout</Link>
-            </li>
-          </ul>
-        </nav>
-      </Sidebar>
-      <Main>{children}</Main>
-    </Container>
+              <li>
+                <Link href="/admin/messages">Messages</Link>
+              </li>
+              {session.user?.name.isAdmin && (
+                <li>
+                  <Link href="/admin/users">Users</Link>
+                </li>
+              )}
+              <li>
+                <Link href="/admin/articles">Articles</Link>
+              </li>
+              <li>
+                <Link href="/admin/settings">Settings</Link>
+              </li>
+              <li onClick={logoutHandler}>
+                <Link href="/admin/logout">Logout</Link>
+              </li>
+            </ul>
+          </nav>
+        </Sidebar>
+        <Main>{children}</Main>
+      </Container>
+      <Footer />
+    </>
   );
 }

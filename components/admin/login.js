d5f80b2e5f8b587d14f8106931b7f8f7f1115221
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 16:46:41 +0300

    redirect to dashboard after login

--- components/admin/login.js
+++ components/admin/login.js
@@ -56,7 +56,7 @@ export default function Login() {
 
   useEffect(() => {
     if (session) {
-      router.replace("/admin/messages");
+      router.replace("/admin/dashboard");
     }
   }, [router, session]);
 

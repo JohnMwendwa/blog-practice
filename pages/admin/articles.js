96e8ccfa7cdc8416bb7175af65dfe8300af8e8fa
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 22:29:08 +0300

    redirect to login page if there is no active session

--- pages/admin/articles.js
+++ pages/admin/articles.js
@@ -3,3 +3,20 @@ import Articles from "../../components/admin/articles";
 export default function articles() {
   return <Articles />;
 }
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+    };
+  }
+}

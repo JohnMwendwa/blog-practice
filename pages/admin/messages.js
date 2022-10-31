21b9403ac3749c592b7c42abf46e6c23a9f1cb50
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 16:39:33 +0300

    hide messages route from unautaurized users

--- pages/admin/messages.js
+++ pages/admin/messages.js
@@ -16,6 +16,13 @@ export async function getServerSideProps(context) {
         permanent: false,
       },
     };
+  } else if (!session.user.name.isAdmin) {
+    return {
+      redirect: {
+        destination: "/admin/dashboard",
+        permanent: false,
+      },
+    };
   } else {
     return {
       props: { session },

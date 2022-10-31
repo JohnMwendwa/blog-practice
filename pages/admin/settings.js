a0408f267e832f9224ce5b38431639513c0c7e47
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 16:33:24 +0300

    redirect user to login page if there is no active session

--- pages/admin/settings.js
+++ pages/admin/settings.js
@@ -1,5 +1,27 @@
+import { getSession } from "next-auth/react";
 import Settings from "../../components/admin/settings";
 
 export default function SettingsPage() {
   return <Settings />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+      redirect: {
+        destination: "/admin/settings",
+        permanent: true,
+      },
+    };
+  }
 }

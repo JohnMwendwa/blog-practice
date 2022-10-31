b1d4ddb82e8aef4ea319894742062f45cbed6e42
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 16:42:22 +0300

    redirect to dashboard after login

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -18,7 +18,7 @@ export async function getServerSideProps(context) {
     return {
       props: { session },
       redirect: {
-        destination: "/admin/messages",
+        destination: "/admin/dashboard",
         permanent: true,
       },
     };

01bea5c1119dc1fd32f4e5909e2b3d7c9d671599
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 10:13:41 +0300

    fetch a single user

---
+++ pages/api/users/user.js
@@ -0,0 +1,33 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return;
+  }
+
+  try {
+    await connectToDatabase();
+
+    const session = await getSession({ req });
+
+    if (!session) {
+      res.status(401).json({ message: "Unauthaurized access!" });
+      return;
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found!" });
+    }
+
+    await closeConnection();
+
+    res.status(200).json(user);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: e.message });
+  }
+}

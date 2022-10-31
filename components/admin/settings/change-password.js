e13bce93960a2de93f1fb3bdad6a9f96d2d60444
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-10-31 14:11:21 +0300

    export the default change-password component

---
+++ components/admin/settings/change-password.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding-left: 20px;
+  margin: 0 auto;
+
+  & label {
+    display: block;
+
+    & input {
+      padding: 5px 10px;
+      margin-bottom: 10px;
+      display: block;
+      border: none;
+      outline: 1px solid;
+    }
+  }
+
+  & button {
+    width: 100%;
+    padding: 8px 0;
+    margin-top: 5px;
+    background-color: blue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+`;
+
+export default function ChangePassword() {
+  recurn(
+    <Container>
+      <h2>Change Password</h2>
+      <form>
+        <label htmlFor="oldPassword">
+          Old password
+          <input type="password" placeholder="old passord" id="oldPassword" />
+        </label>
+
+        <label htmlFor="newPassword">
+          New password
+          <input type="password" placeholder="new password" id="newPassword" />
+        </label>
+
+        <label htmlFor="consfirmPassword">
+          Confirm new password
+          <input
+            type="password"
+            placeholder="confirm new password"
+            id="confirmPassword"
+          />
+        </label>
+
+        <button>Update</button>
+      </form>
+    </Container>
+  );
+}

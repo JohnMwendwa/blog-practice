0fd84533ab3e91227c0242fa044b7281cd1d4953
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-02 21:15:57 +0300

    create a middleware to sanitize markdown data

--- helpers/db/models/post.js
+++ helpers/db/models/post.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
+import { marked } from "marked";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
+
 const { Schema } = mongoose;
+const window = new JSDOM("").window;
+const purify = DOMPurify.addHook(window);
 
 const postSchema = new Schema({
   title: {
@@ -19,6 +25,14 @@ const postSchema = new Schema({
   },
   nLikes: Number,
   nComments: Number,
+});
+
+postSchema.pre("validate", async function (next) {
+  const post = this;
+  if (post.markdown) {
+    post.markdown = purify.sanitize(marked.parse(post.markdown));
+  }
+  next();
 });
 
 const Post = mongoose.models.Post || mongoose.model("Post", postSchema);

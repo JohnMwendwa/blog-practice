9091f45da3ae3d2a45874c09f2845b5ecab07f9a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-03 15:40:08 +0300

    create a link to the new article page

--- components/admin/articles/index.js
+++ components/admin/articles/index.js
@@ -1,9 +1,37 @@
-import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+`;
+
+const Btn = styled.button`
+  padding: 5px 10px;
+  margin-bottom: 20px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(c) => c.theme.colors.ui.secondary};
+  color: white;
+  outline: 1px solid ${(c) => c.theme.colors.ui.secondary};
+  font-size: 1.25rem;
+
+  & span {
+    font-weight: bold;
+    padding: 3px;
+  }
+`;
 
 export default function Articles() {
   return (
-    <div>
+    <Wrapper>
       <h2>My Articles</h2>
-    </div>
+      <Link href="/admin/articles/new">
+        <a>
+          <Btn>
+            <span>+</span> Create New
+          </Btn>
+        </a>
+      </Link>
+    </Wrapper>
   );
 }

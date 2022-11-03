1c240d56ff52683b9d57d30b4453d35bc1b614ea
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-03 08:43:19 +0300

    change posts api route to a get route

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -2,25 +2,17 @@ import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
 import Post from "../../../helpers/db/models/post";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
+  if (req.method !== "GET") {
     return;
   }
 
   try {
     await connectToDatabase();
 
-    const post = new Post({
-      title: req.body.title,
-      description: req.body.description,
-      markdown: req.body.markdown,
-    });
-
-    await post.save();
+    const posts = await Post.find({});
     await closeConnection();
 
-    res.status(201).json({
-      message: "New article created",
-    });
+    res.status(200).json(posts);
   } catch (e) {
     res.status(500).json({
       error: e.message,

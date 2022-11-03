1f6777ea051ec8d7dc3128fbecff5d36da9896da
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-03 15:14:06 +0300

    only allow authenticated users access the new articles route

--- pages/admin/articles/new.js
+++ pages/admin/articles/new.js
@@ -1,5 +1,30 @@
+import { getSession } from "next-auth/react";
 import NewArticle from "../../../components/admin/articles/new-article";
 
 export default function NewArticlePage() {
   return <NewArticle />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  } else if (!session.user.name.isAuthenticated) {
+    return {
+      redirect: {
+        destination: "/admin/dashboard",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: { session },
+    };
+  }
 }

dda6034c85a3b46f2029a7f2e33ecc14e43d1a35
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-05 21:46:02 +0300

    setup api route to post and update profile photos

---
+++ pages/api/users/profile.js
@@ -0,0 +1,54 @@
+import nextConnect from "next-connect";
+import { getSession } from "next-auth/react";
+import sharp from "sharp";
+
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+import { upload } from "../../../helpers/upload";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler = nextConnect({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Something went wrong" });
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).json({ error: "Page Not Found" });
+  },
+})
+  .use(upload.single("avatar"))
+  .post(async (req, res) => {
+    try {
+      await connectToDatabase();
+
+      const session = await getSession({ req });
+
+      if (!session) {
+        await closeConnection();
+        res.status(401).json({ message: "Unauthaurized access!" });
+        return;
+      }
+
+      const avatar = await sharp(req.file.buffer)
+        .resize({ width: 80, height: 80 })
+        .png()
+        .toBuffer();
+
+      const user = await User.findOne({ email: session.user.email });
+      user.avatar = avatar;
+      await user.save();
+
+      await closeConnection();
+
+      res.status(201).json({ message: "Profile photo updated" });
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ error: e.message });
+    }
+  });
+export default handler;

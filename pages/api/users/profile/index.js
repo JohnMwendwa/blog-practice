48bd490890f0e8ebe5235a0f4cf30cd1f3c0185c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-06 08:01:55 +0300

    move profile.js file into profile folder and rename it to index.js file

--- pages/api/users/profile/index.js
+++ pages/api/users/profile/index.js
@@ -2,9 +2,9 @@ import nextConnect from "next-connect";
 import { getSession } from "next-auth/react";
 import sharp from "sharp";
 
-import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
-import User from "../../../helpers/db/models/user";
-import { upload } from "../../../helpers/upload";
+import { connectToDatabase, closeConnection } from "../../../../helpers/db/db";
+import User from "../../../../helpers/db/models/user";
+import { upload } from "../../../../helpers/upload";
 
 export const config = {
   api: {
@@ -49,6 +49,28 @@ const handler = nextConnect({
     } catch (e) {
       console.log(e.message);
       res.status(500).json({ error: e.message });
+    }
+  })
+  .get(async (req, res) => {
+    try {
+      await connectToDatabase();
+
+      const session = await getSession({ req });
+
+      if (!session) {
+        await closeConnection();
+        res.status(401).json({ message: "Unauthaurized access!" });
+        return;
+      }
+
+      const user = await User.findOne({ email: session.user.email });
+      res.setHeader("Content-Type", "image/jpg");
+      await closeConnection();
+      console.log(user.avatar);
+      res.send(user.avatar);
+    } catch (e) {
+      console.log(e.message);
+      res.status(404).json({ error: e.message });
     }
   });
 export default handler;

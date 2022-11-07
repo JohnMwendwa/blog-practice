8abe990350dfa4ca129768c34dd442787857350b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-07 14:54:42 +0300

    setup api route to serve posts images

--- pages/api/posts/[id]/image.js
+++ pages/api/posts/[id]/image.js
@@ -1,5 +1,25 @@
+import { connectToDatabase, closeConnection } from "../../../../helpers/db/db";
+import Post from "../../../../helpers/db/models/post";
+
 export default async function handler(req, res) {
   if (req.method !== "GET") {
     return;
+  }
+
+  try {
+    await connectToDatabase();
+    const post = await Post.findById({ _id: req.query.id });
+
+    if (!post) {
+      await closeConnection();
+      res.status(404).json({ error: "Image Not Found!" });
+      return;
+    }
+
+    res.setHeader("Content-Type", "image/png");
+
+    res.send(post.image);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 }

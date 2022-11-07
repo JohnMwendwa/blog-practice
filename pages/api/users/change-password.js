c48a31765b52e393473d28bd26ba09a14d590a34
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-07 11:42:26 +0300

    setup api route for changing user password

---
+++ pages/api/users/change-password.js
@@ -0,0 +1,60 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import { hashPassword, verifyPassword } from "../../../helpers/db/auth";
+import User from "../../../helpers/db/models/user";
+
+export default async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  try {
+    await connectToDatabase();
+    const session = await getSession({ req });
+
+    if (!session) {
+      await closeConnection();
+      res.status(401).json({ error: "Unauthorized access" });
+      return;
+    }
+
+    const { oldPassword, newPassword, confirmPassword } = req.body;
+
+    if (!oldPassword.trim() || !newPassword.trim()) {
+      await closeConnection();
+      res.status(400).json({
+        error: "Cannot set empty password",
+      });
+      return;
+    }
+
+    if (newPassword !== confirmPassword) {
+      await closeConnection();
+      res
+        .status(400)
+        .json({ error: "New password and confirm password don't match" });
+      return;
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+
+    const isMatch = await verifyPassword(oldPassword, user.password);
+
+    if (!isMatch) {
+      await closeConnection();
+      res.status(400).json({ error: "Invalid old password" });
+      return;
+    }
+
+    const newHashedPassword = await hashPassword(newPassword);
+
+    user.password = newHashedPassword;
+    await user.save();
+    await closeConnection();
+
+    res.status(200).json({ message: "Password updated" });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ error: e.message });
+  }
+}

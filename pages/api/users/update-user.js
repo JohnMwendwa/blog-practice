727d1e76bf0d6b56af20557aad41299f1ee60433
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-07 14:19:35 +0300

    setup api route to update user details

--- pages/api/users/update-user.js
+++ pages/api/users/update-user.js
@@ -1,5 +1,37 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+
 export default async function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
+  }
+
+  try {
+    await connectToDatabase();
+    const session = await getSession({ req });
+
+    if (!session) {
+      await closeConnection();
+      res.status(401).json({ error: "Unauthaurized access" });
+      return;
+    }
+
+    const { firstName, lastName, email } = req.body;
+
+    const user = await User.findOne({ email: session.user.email });
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+
+    await user.save();
+
+    await closeConnection();
+    res.status(200).json({
+      message: "Details updated",
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 }

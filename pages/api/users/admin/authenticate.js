a328565e1c329cc1c5abde86751f35c189b2f02c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-10 15:08:06 +0300

    setup api route for admins to authenticate users

--- pages/api/users/admin/authenticate.js
+++ pages/api/users/admin/authenticate.js
@@ -1,5 +1,38 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../../helpers/db/db";
+import User from "../../../../helpers/db/models/user";
+
 export default async function handler(req, res) {
-  if (!req.method !== "PATCH") {
+  if (req.method !== "PATCH") {
     return;
+  }
+
+  try {
+    await connectToDatabase();
+
+    const session = await getSession({ req });
+
+    if (!session || !session.user.name.isAdmin) {
+      await closeConnection();
+      res.status(401).json({ error: "Unauthorized Access!" });
+      return;
+    }
+
+    const user = await User.findById({ _id: req.body.id });
+
+    if (!user) {
+      await closeConnection();
+      res.status(404).json({ error: "User Not Found" });
+      return;
+    }
+
+    user.isAuthenticated = !user.isAuthenticated;
+    await user.save();
+
+    await closeConnection();
+
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 }

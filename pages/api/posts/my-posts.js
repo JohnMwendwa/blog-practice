291fb0a411704325632450645c65357e6b0428b8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-12 17:45:28 +0300

    return user specific posts

--- pages/api/posts/my-posts.js
+++ pages/api/posts/my-posts.js
@@ -26,6 +26,8 @@ export default async function handler(req, res) {
       return;
     }
 
+    const posts = await Post.find({ author: user._id });
+
     await closeConnection();
 
     res.status(200).json(posts);

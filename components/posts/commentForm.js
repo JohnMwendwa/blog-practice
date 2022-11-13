36c1e2b797a0dc4162352fc3e23bc7166e0ec137
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-13 09:39:20 +0300

    export the default commentform component

---
+++ components/posts/commentForm.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Form = styled.form``;
+const TextArea = styled.textarea``;
+const Btn = styled.button``;
+
+export default function commentForm() {
+  return (
+    <Form>
+      commentForm
+      <TextArea />
+      <Btn>Post</Btn>
+    </Form>
+  );
+}

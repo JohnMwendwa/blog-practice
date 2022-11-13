61eddcdaeeec118f78264a93487a770ec718f683
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-13 15:20:16 +0300

    create a function for grouping comments by their parent ids

--- components/contexts/PostContext.js
+++ components/contexts/PostContext.js
@@ -1,7 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState, useMemo } from "react";
 
 const PostContext = createContext();
 
 export function PostProvider({ children }) {
+  const [comments, setComments] = useState(null);
+
+  const commentByParentId = useMemo(() => {
+    if (comments === null) return [];
+    const group = {};
+
+    comments.forEach((comment) => {
+      group[comment.parentId] || [];
+      group[comment.parentId].push(comment);
+    });
+
+    return group;
+  }, [comments]);
+
   return <PostContext.Provider value={{}}>{children}</PostContext.Provider>;
 }

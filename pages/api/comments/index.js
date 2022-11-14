1919d444ce64a12cf589fd40838450e9e59f0b53
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-15 01:43:58 +0300

    create api route for fetching comments

---
+++ pages/api/comments/index.js
@@ -0,0 +1,24 @@
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+import Comment from "../../../helpers/db/models/comment";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  try {
+    await connectToDatabase();
+
+    const comments = await Comment.find({ postId: req.body.id }).populate(
+      "user",
+      "firstName lastName",
+      User
+    );
+
+    await closeConnection();
+    res.status(200).json(comments);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}

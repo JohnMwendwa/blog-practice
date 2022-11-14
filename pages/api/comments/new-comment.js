2fd2b8c88060ba634bbd8bd9318a57529291ddbe
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-14 23:29:04 +0300

    create an api route to post new comments

---
+++ pages/api/comments/new-comment.js
@@ -0,0 +1,37 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+import Comment from "../../../helpers/db/models/comment";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  try {
+    await connectToDatabase();
+    const session = await getSession({ req });
+
+    if (!session) {
+      await closeConnection();
+      res.redirect("/admin/login");
+      return;
+    }
+
+    const { postId, message, parentId } = req.body;
+
+    const user = await User.findOne({ email: session.user.email });
+    const newComment = new Comment({
+      postId,
+      parentId,
+      user: user._id,
+      body: message,
+    });
+    const comment = await newComment.save();
+
+    await closeConnection();
+    res.status(201).json(comment);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}

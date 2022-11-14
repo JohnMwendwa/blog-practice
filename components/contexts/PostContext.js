22302d97327d26b66fa3683ebd4347dbf5f6c9ed
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-15 00:51:28 +0300

    set the post id

--- components/contexts/PostContext.js
+++ components/contexts/PostContext.js
@@ -6,14 +6,14 @@ export const usePost = () => {
   return useContext(PostContext);
 };
 
-export function PostProvider({ children }) {
+export function PostProvider({ children, post }) {
   const [comments, setComments] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {}, [comments]);
 
-  const onSendComment = async (postId, message, parentId) => {
+  const onSendComment = async (message, parentId, postId = post._id) => {
     setLoading(true);
 
     const response = await fetch("/api/comments/new-comment", {
@@ -37,11 +37,10 @@ export function PostProvider({ children }) {
   };
 
   const commentByParentId = useMemo(() => {
-    if (comments === null) return [];
     const group = {};
 
     comments.forEach((comment) => {
-      group[comment.parentId] || [];
+      group[comment.parentId] ||= [];
       group[comment.parentId].push(comment);
     });
 

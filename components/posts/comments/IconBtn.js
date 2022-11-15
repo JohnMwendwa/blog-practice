c0237de811c669a1e0a378d6bdf033d916ced3f9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-15 09:46:44 +0300

    style the comment icons

--- components/posts/comments/IconBtn.js
+++ components/posts/comments/IconBtn.js
@@ -1,56 +1,64 @@
 import styled from "styled-components";
 
 const Btn = styled.button`
---hue: 235;
---color: hsl(var(--hue), 100%, 67%);
-border: none;
-border-radius: .5em;
-font-size: .75em;
-background: none;
-color: var(--color);
-padding: .25em;
-display: flex;
-align-items: center;
-cursor: pointer;
+  --hue: 235;
+  --color: hsl(var(--hue), 100%, 67%);
+  border: none;
+  border-radius: 0.5em;
+  font-size: 0.75em;
+  background: none;
+  color: var(--color);
+  padding: 0.25em;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
 
-& :hover,:focus-visible{
-    --color: hsl(var(--hue), 100%, 74%);   
-}
+  :hover,
+  :focus-visible {
+    --color: hsl(var(--hue), 100%, 74%);
+  }
 
-& .icon-btn-active,
-.icon-btn.danger {
-  --hue: 0;
-}
+  .icon-btn-active,
+  .icon-btn.danger {
+    --hue: 0;
+  }
 
-& .icon-btn-active{
+  .icon-btn-active {
     position: relative;
 
-    &::before{
-        content: "\00D7";
-        position: absolute;
-        font-size: .75em;
-        width: 1em;
-        height: 1em;
-        color: white;
-        background-color: var(--color);
-        border-radius: 50%;
-        bottom: .1em;
-        right: .1em;
+    ::before {
+      content: "00D7";
+      position: absolute;
+      font-size: 0.75em;
+      width: 1em;
+      height: 1em;
+      color: white;
+      background-color: var(--color);
+      border-radius: 50%;
+      bottom: 0.1em;
+      right: 0.1em;
     }
-}
-  & .icon-mg {
+  }
+
+  .icon-btn-active .icon-mg {
     margin-right: 0.25em;
   }
 `;
 
-export default function IconBtn({ Icon, isActive, color, children, ...props }) {
+export default function IconBtn({
+  Icon,
+  color,
+  children,
+  isActive = true,
+  ...props
+}) {
   return (
-    <Btn
-      className={`${isActive ? "icon-btn-active" : ""} ${color || ""}`}
-      {...props}
-    >
+    <Btn {...props}>
       <span className={`${children !== null ? "icon-mg" : ""}`}>
-        <Icon />
+        <Icon
+          color={color}
+          className={`${isActive ? "icon-btn-active" : ""}`}
+        />
       </span>
 
       {children}

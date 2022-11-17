8e2524f4735fc792cccda83e8978aa0a8116ff37
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-17 15:13:08 +0300

    redirect to admin dashboard once there is an active session

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,5 +1,22 @@
+import { getSession } from "next-auth/react";
 import Login from "../../components/admin/login";
 
 export default function LoginPage() {
   return <Login />;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/admin/dashboard",
+        permanent: true,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
 }

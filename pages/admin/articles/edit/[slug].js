dfd74cb267f2717eb6ea93dbcac77819b6413281
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-17 15:15:36 +0300

    allow only authenticated users to edit a post

--- pages/admin/articles/edit/[slug].js
+++ pages/admin/articles/edit/[slug].js
@@ -1,3 +1,30 @@
-export default function EditArticlePage() {
+import { getSession } from "next-auth/react";
+
+export default function EditArticlePage(props) {
   return <div>Editing</div>;
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  }
+  if (!session.user.isAuthenticated) {
+    return {
+      redirect: {
+        destination: "/admin/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 }

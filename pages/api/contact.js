916c413bb350a402205b356f2340f8605fc6db56
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-18 08:17:14 +0300

    sort the messages before returning by the date they ere created

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -70,7 +70,7 @@ export default async function handler(req, res) {
       });
     }
 
-    const messages = await Message.find({});
+    const messages = await Message.find({}).sort({ createdAt: -1 });
 
     await closeConnection();
     res.status(200).json(messages);

cca13635706f927f82ef7c5a7950d029fa6c9992
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-18 08:10:28 +0300

    change the message error object to an errorr message object

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -14,14 +14,14 @@ export default async function handler(req, res) {
     const session = await getSession({ req });
 
     if (!session.user.isAdmin) {
-      res.status(401).json({ message: "Unauthaurized access!" });
+      res.status(401).json({ error: "Unauthaurized access!" });
       return;
     }
 
     const user = await User.findById({ _id: id });
 
     if (!user) {
-      return res.status(404).json({ message: "User Not Found!" });
+      return res.status(404).json({ error: "User Not Found!" });
     }
 
     await closeConnection();
@@ -29,6 +29,6 @@ export default async function handler(req, res) {
     res.status(200).json(user);
   } catch (e) {
     console.log(e);
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ error: e.message });
   }
 }

a63aa1a6a2ba1bc90413ba8266e6b97a4c884e60
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-18 09:27:53 +0300

    setup api route to delete a user account

---
+++ pages/api/users/delete-user.js
@@ -0,0 +1,29 @@
+import { getSession } from "next-auth/react";
+import { connectToDatabase, closeConnection } from "../../../helpers/db/db";
+import User from "../../../helpers/db/models/user";
+
+export default async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+
+  try {
+    await connectToDatabase();
+    const session = await getSession({ req });
+
+    if (!session) {
+      await closeConnection();
+      res.status(401).json({ error: "Unauthorized access" });
+      return;
+    }
+
+    await User.findOneAndRemove({ email: session.user.email });
+
+    await closeConnection();
+    res.status(200).json({
+      message: "Account Deleted!",
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}

7881796b9300e5d6334c7e1983778a6dde357097
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-18 09:51:54 +0300

    change user remove method in order to trigger the remove middleware to clean all user posts and comments

--- pages/api/users/delete-user.js
+++ pages/api/users/delete-user.js
@@ -17,7 +17,8 @@ export default async function handler(req, res) {
       return;
     }
 
-    await User.findOneAndRemove({ email: session.user.email });
+    const user = await User.findOne({ email: session.user.email });
+    await user.remove();
 
     await closeConnection();
     res.status(200).json({

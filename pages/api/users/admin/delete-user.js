70c80c08f05b1973fa0e8f20766181cfc258e9d1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-11-21 23:06:27 +0300

    create an api route for deleting users

--- pages/api/users/admin/delete-user.js
+++ pages/api/users/admin/delete-user.js
@@ -6,4 +6,69 @@ export default async function handler(req, res) {
   if (req.method !== "DELETE") {
     return;
   }
+
+  try {
+    await connectToDatabase();
+
+    const session = await getSession({ req });
+
+    if (!session || !session.user.isAdmin) {
+      await closeConnection();
+      res.status(401).json({ error: "Unauthorized Access!" });
+      return;
+    }
+
+    const user = await User.findById({ _id: req.body.id }).select(
+      "+superAdmin"
+    );
+    const currentUser = await User.findOne({ email: session.user.email });
+
+    if (!user) {
+      await closeConnection();
+      res.status(404).json({ error: "User Not Found" });
+      return;
+    }
+
+    // Disallow the super admin from deleting their account
+    if (user.superAdmin && currentUser.superAdmin) {
+      await closeConnection();
+      res
+        .status(401)
+        .json({ error: "Sorry Boss,you can't delete your account!" });
+      return;
+    }
+
+    // Super admin can delete an admin
+    if (user.isAdmin && currentUser.superAdmin) {
+      await user.remove();
+      await closeConnection();
+      res.status(201).json(user);
+
+      return;
+    }
+
+    //An admin can't delete super admin account
+    if (user.superAdmin) {
+      await closeConnection();
+      res.status(401).json({ error: "User above your clearance!" });
+      return;
+    }
+
+    // An admin can't delete another admin
+    if (user.isAdmin) {
+      await closeConnection();
+      res.status(401).json({ error: "You can't remove another admin!" });
+      return;
+    }
+
+    await user.remove();
+
+    await closeConnection();
+
+    res.status(201).json({
+      message: "Account Deleted!",
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 }

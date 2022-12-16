bd1171b80d655662005de340ca81f13653978607
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-16 15:11:53 +0300

    use unstable_getServerSession instead of getSession

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,4 +1,6 @@
-import { getSession } from "next-auth/react";
+import { authOptions } from "../api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next";
+
 import Dashboard from "../../components/admin/dashboard";
 
 export default function DashboardPage() {
@@ -6,7 +8,11 @@ export default function DashboardPage() {
 }
 
 export async function getServerSideProps(context) {
-  const session = await getSession({ req: context.req });
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
 
   if (!session) {
     return {

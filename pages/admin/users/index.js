83498c28d52ddfe504ed9f0f899ee9f5a2abae7a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-16 15:13:36 +0300

    use unstable_getServerSession instead of getSession

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,4 +1,6 @@
-import { getSession } from "next-auth/react";
+import { authOptions } from "../../api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next";
+
 import Users from "../../../components/admin/users/index";
 
 export default function UsersPage() {
@@ -6,7 +8,11 @@ export default function UsersPage() {
 }
 
 export async function getServerSideProps(context) {
-  const session = await getSession({ req: context.req });
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
 
   if (!session) {
     return {

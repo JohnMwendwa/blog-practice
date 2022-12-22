cbb95a8c5551a644cf1dccb9f1d7568eed13fc6f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-22 13:46:25 +0300

    close db connection if there are errors

--- pages/api/posts/delete-post.js
+++ pages/api/posts/delete-post.js
@@ -46,6 +46,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({});
   } catch (e) {
+    await closeConnection();
     res.status(500).json({
       error: e.message,
     });

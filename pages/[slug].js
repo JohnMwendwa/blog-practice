66c8d687081564cbc40bebff2325fdd5a9608422
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2022-12-24 17:47:46 +0300

    change fallback to blocking to cater for new articles

--- pages/[slug].js
+++ pages/[slug].js
@@ -40,7 +40,7 @@ export async function getStaticPaths() {
     paths: slugs.map((post) => ({
       params: { slug: post.slug },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 }
 
